a78e2059bdc1b1e13dd13644b110bb0b350ae6b5
DrDeckto <fynjy138@gmail.com>
2022-04-06 15:43:47 +0300

    Добавьте файлы проекта.

---
+++ Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RationalNumbers one = new RationalNumbers(4, 5);
+            RationalNumbers two = new RationalNumbers(1, 7);
+
+            one += two;
+
+            one.Show();
+        }
+    }
+}
